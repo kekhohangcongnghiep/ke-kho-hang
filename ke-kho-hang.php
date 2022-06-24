0f9e44cd3f3b25aa7da62b2c175d8f34668edc28
Kệ kho hàng công nghiệp Onetech <108117054+kekhohangcongnghiep@users.noreply.github.com>
2022-06-24 14:12:01 +0700

    Create ke-kho-hang.php

---
+++ ke-kho-hang.php
@@ -0,0 +1,9 @@
+<html>
+  <title>Kệ kho hàng Onetech</title>
+  <body>
+<?php
+echo '<center><h1>Nhà sản xuất giá kệ kho hàng giá rẻ, chất lượng số 1 Việt Nam</h1></center>';
+    
+?>
+  </body>
+</html>
